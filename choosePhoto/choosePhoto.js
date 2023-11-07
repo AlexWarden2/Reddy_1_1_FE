686b46fa463f162e88cbebcc2b9364765d8b6b94
Alex Warden <alex.wardenn@gmail.com>
2023-11-07 18:35:26 +0000

    in progress - testing will come back to this

--- choosePhoto/choosePhoto.js
+++ choosePhoto/choosePhoto.js
@@ -1,51 +1,55 @@
-
-
-const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
-const answerButtons = document.getElementById('answer-buttons')
-const imgFromURL = document.getElementById('image')
-
-
+const startButton = document.getElementById('start-btn')
 
 startButton.addEventListener('click', startGame)
 
-let answers = []
-let correct = []
-let imageURL = []
+const flagImage = document.getElementById('image')
 
+const button1 = document.getElementById('btn1')
+const button2 = document.getElementById('btn2')
+const button3 = document.getElementById('btn3')
+const button4 = document.getElementById('btn4')
+
+// button1.addEventListener('click', checkAnswer)
+// button2.addEventListener('click', checkAnswer)
+// button3.addEventListener('click', checkAnswer)
+// button4.addEventListener('click', checkAnswer)
+
+
+let answers = []
+let shuffledAnswers = []
+let correct = []
 
 function getRandQuestion() {
-    fetch("http://localhost:3000/data/choosePhoto")
-    .then(res => res.json())
-    .then(data => { fetchInfo(data) })
-
-    function fetchInfo(data){
-
-        correct.push(data.correct)
-        answers.push(data.correct)
-        imageURL.push(data.question)
-
-        for (i=0; i<3; i++){
-            answers.push(data.incorrect[i])
-        }
-    }
+    fetch("https://reddy-1-1-be.onrender.com/data/choosePhoto")
+     .then(res => res.json())
+     .then(data => fetchInfo(data))
 }
-
 getRandQuestion()
-console.log(answers)
-console.log(correct)
-console.log(imageURL)
+
+function fetchInfo(data){
+    correct = data.correct
+    console.log(correct)
+
+    flagImage.src = data.question;
+    answers = data.incorrect
+    answers.push(data.correct)
+    console.log(answers)
+
+    shuffledAnswers = answers.sort(() => Math.random() -0.5)
+    console.log(shuffledAnswers)
+
+    btn1.textContent = shuffledAnswers[0]
+    btn2.textContent = shuffledAnswers[1]
+    btn3.textContent = shuffledAnswers[2]
+    btn4.textContent = shuffledAnswers[3]
+}
+fetchInfo()
 
 
 function startGame() {
-    console.log('Started')
+    // console.log('Started')
     startButton.classList.add('hide')
     questionContainerElement.classList.remove('hide')
-
 }
-
-// function showFlag() {
-//     imgFromURL.src = imageURL[0]
-// }
-// showFlag()
 
