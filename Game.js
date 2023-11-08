e9b76e0a115be2ff2f70a2a83288ef96936394a6
Alex Warden <alex.wardenn@gmail.com>
2023-11-08 17:04:18 +0000

    test score system

--- Game.js
+++ Game.js
@@ -4,3 +4,32 @@
 
 //after run calculate score after 5 mini games
 
+const next = document.querySelector("#create-score");
+
+next.addEventListener('click', createNewScore);
+
+async function createNewScore(e) {
+    e.preventDefault()
+
+    const score = {
+        id: e.length + 1,
+        score: e.length + 1
+    }
+
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(score)
+    }
+
+    const response = await fetch("http://localhost:3000/scores", options);
+
+    if (response.ok) {
+        alert("Score added.");
+    }
+}
+
+
+
