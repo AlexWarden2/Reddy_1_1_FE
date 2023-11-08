096248795c34e881b327d314b574d93435a71ca8
jarvis <jsd.ditcher@outlook.com>
2023-11-08 13:12:22 +0000

    add functionality to 2truth-1lie minigame, fill up .js file with relevent working functions to achive the desired functionality of the minigame

--- 2truth-1lie/2truth-1lie.js
+++ 2truth-1lie/2truth-1lie.js
@@ -1,3 +1,13 @@
+const button1 = document.getElementById('btn-1')
+const button2 = document.getElementById('btn-2')
+const button3 = document.getElementById('btn-3')
+
+const question = document.getElementById('question')
+const nextQuestionBtn = document.getElementById('next-btn');
+
+const buttons = document.querySelectorAll('.answer-button')
+
+let shuffledAnswers = []
 
 
 function fetchData(){
@@ -5,3 +15,46 @@ function fetchData(){
     .then(res => res.json())
     .then(data => startGame(data))
 }
+
+function startGame(data){
+
+    shuffleData(data)
+    // console.log(data)
+    // console.log(shuffledAnswers)
+    button1.textContent = shuffledAnswers[0].text 
+    button2.textContent = shuffledAnswers[1].text
+    button3.textContent = shuffledAnswers[2].text
+    
+    button1.bool = shuffledAnswers[0].correct
+    button2.bool = shuffledAnswers[1].correct
+    button3.bool = shuffledAnswers[2].correct
+
+    buttons.forEach(button => {
+        button.addEventListener('click', buttonClickHandler)
+    })
+
+    nextQuestionBtn.addEventListener('click', () => {
+        location.reload()
+    })
+}
+
+function shuffleData(data){
+    shuffledAnswers = chance.shuffle(data.answers)
+}
+
+function buttonClickHandler(){
+    if (this.bool === true){
+        // console.log('Correct')
+        // buttons.forEach(button => {
+        //     button.remove()
+        // })
+        question.textContent = 'Congratulations, You guess right'
+        nextQuestionBtn.style.display = 'block'
+
+    } else {
+        console.log('Incorrect')
+        return false
+    }
+}
+
+fetchData()
