cae4b97ec416db9215b66bce121aacb7daefe359
asiek728 <asiek728@gmail.com>
2023-11-08 13:36:05 +0000

    add basic guess date game

---
+++ guessDate/guessDate.js
@@ -0,0 +1,56 @@
+const questionContainerElement = document.getElementById('question-container')
+const question = document.getElementById('question')
+const startButton = document.getElementById('start-btn')
+const buttons = document.querySelectorAll('.btn')
+const button1 = document.getElementById('btn1')
+const button2 = document.getElementById('btn2')
+const button3 = document.getElementById('btn3')
+const button4 = document.getElementById('btn4')
+
+button1.addEventListener('click', checkAnswer)
+button2.addEventListener('click', checkAnswer)
+button3.addEventListener('click', checkAnswer)
+button4.addEventListener('click', checkAnswer)
+startButton.addEventListener('click', startGame)
+
+getRandQuestion()
+
+function getRandQuestion() {
+    fetch("https://reddy-1-1-be.onrender.com/data/dates")
+     .then(res => res.json())
+     .then(data => fetchInfo(data))
+}
+
+function fetchInfo(data){
+    question.textContent = data.question;
+    answers = data.answers
+    shuffledAnswers = answers.sort(() => Math.random() -0.5)
+
+    button1.textContent = shuffledAnswers[0].text 
+    button2.textContent = shuffledAnswers[1].text
+    button3.textContent = shuffledAnswers[2].text
+    button4.textContent = shuffledAnswers[3].text
+    button1.bool = shuffledAnswers[0].correct
+    button2.bool = shuffledAnswers[1].correct
+    button3.bool = shuffledAnswers[2].correct
+    button4.bool = shuffledAnswers[3].correct
+}
+
+function startGame() {
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+}
+
+buttons.forEach(button => {
+        button.addEventListener('click', checkAnswer)
+    })
+
+function checkAnswer() {
+   if (this.bool === true){
+    console.log(`correct`)
+    this.style.backgroundColor = "green";
+   } else {
+    console.log(`false`)
+    this.style.backgroundColor = "red";
+   } 
+}
