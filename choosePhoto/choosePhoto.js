30966c97512425412313e8a9b0c00de199454664
Alex Warden <alex.wardenn@gmail.com>
2023-11-08 10:59:00 +0000

    add choose photo functionality working

--- choosePhoto/choosePhoto.js
+++ choosePhoto/choosePhoto.js
@@ -1,6 +1,7 @@
 const questionContainerElement = document.getElementById('question-container')
 const startButton = document.getElementById('start-btn')
 
+
 startButton.addEventListener('click', startGame)
 
 const flagImage = document.getElementById('image')
@@ -10,43 +11,45 @@ const button2 = document.getElementById('btn2')
 const button3 = document.getElementById('btn3')
 const button4 = document.getElementById('btn4')
 
-// button1.addEventListener('click', checkAnswer)
-// button2.addEventListener('click', checkAnswer)
-// button3.addEventListener('click', checkAnswer)
-// button4.addEventListener('click', checkAnswer)
+button1.addEventListener('click', checkAnswer)
+button2.addEventListener('click', checkAnswer)
+button3.addEventListener('click', checkAnswer)
+button4.addEventListener('click', checkAnswer)
 
-
-// let answers = []
-// let shuffledAnswers = []
-// let correct = []
 
 function getRandQuestion() {
-    fetch("https://reddy-1-1-be.onrender.com/data/choosePhoto")
+    fetch("http://localhost:3000/data/choosePhoto")
      .then(res => res.json())
      .then(data => fetchInfo(data))
 }
 getRandQuestion()
 
+
 function fetchInfo(data){
-    correct = data.correct
-    console.log(correct)
 
     flagImage.src = data.question;
-    answers = data.incorrect
-    answers.push(data.correct)
-    console.log(answers)
+
+    answers = data.answers
 
     shuffledAnswers = answers.sort(() => Math.random() -0.5)
     console.log(shuffledAnswers)
 
-    btn1.textContent = shuffledAnswers[0]
-    btn2.textContent = shuffledAnswers[1]
-    btn3.textContent = shuffledAnswers[2]
-    btn4.textContent = shuffledAnswers[3]
+    button1.textContent = shuffledAnswers[0].text 
+    button2.textContent = shuffledAnswers[1].text
+    button3.textContent = shuffledAnswers[2].text
+    button4.textContent = shuffledAnswers[3].text
+
+    button1.bool = shuffledAnswers[0].correct
+    button2.bool = shuffledAnswers[1].correct
+    button3.bool = shuffledAnswers[2].correct
+    button4.bool = shuffledAnswers[3].correct
+
+    // console.log(button1.bool)
+    // console.log(button2.bool)
+    // console.log(button3.bool)
+    // console.log(button4.bool)
 }
 fetchInfo()
-
-console.log(answers)
 
 
 function startGame() {
@@ -55,3 +58,17 @@ function startGame() {
     questionContainerElement.classList.remove('hide')
 }
 
+const buttons = document.querySelectorAll('.btn')
+
+
+buttons.forEach(button => {
+        button.addEventListener('click', checkAnswer)
+    })
+
+function checkAnswer() {
+   if (this.bool === true){
+    console.log(`correct`)
+   } else {
+    console.log(`false`)
+   } 
+}
