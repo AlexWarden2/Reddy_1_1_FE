a6364aae3ceae1851d55a0e8a8d4a9f987af65dd
Alex Warden <alex.wardenn@gmail.com>
2023-11-08 18:12:04 +0000

    add game files for score

---
+++ game.js
@@ -0,0 +1,32 @@
+// need to be able to run each file once - break condition after each - paybe through popup/ alert
+// store output
+//then need to be able to run next file in random list
+
+//after run calculate score after 5 mini games
+
+const next = document.querySelector("#create-score");
+
+next.addEventListener('click', createNewScore);
+
+async function createNewScore(e) {
+    e.preventDefault()
+
+    const score = {
+        id: e.length + 1,
+        score: e.length + 1
+    }
+
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(score)
+    }
+
+    const response = await fetch("https://reddy-1-1-be.onrender.com/scores", options);
+
+    if (response.ok) {
+        alert("Score added.");
+    }
+}
